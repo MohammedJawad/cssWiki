6f5369b5661584b5ec0c3e05aaade31d8505492d
mjawad <mohammed.jawad.1995@hotmail.com>
2018-03-28 11:19:37 -0400

    ajout de l'index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8"/>
+    <title>cssWiki</title>
+</head>
+<body>
+<div id="wrapper">
+    <header>
+        <h1>cssWiki</h1>
+    </header>
+
+    <main>
+    </main>
+
+    <footer>
+    </footer>
+</div>
+</body>
+</html>
