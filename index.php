089b1e2038fa46fa160189e7f617738509f30a75
bleyvamoo <blancafer_leyva@hotmail.com>
2018-04-04 09:40:16 -0400

    Css folder

--- index.php
+++ index.php
@@ -6,19 +6,38 @@
 <head>
     <meta charset="UTF-8"/>
     <title>cssWiki</title>
+    <link href="style/main.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
 </head>
 <body>
-<div id="wrapper">
+
     <header>
-        <h1>cssWiki</h1>
+        <img src="images/csswiki_logo2.png" href="index.php" alt="company.logo" id="logo">
+
+
+            <nav id="main-menu">
+            <ul>
+
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="catalogue.php">Catalogue</a></li>
+                <li><a href="contact.php">Contactez-Nous</a></li>
+
+
+            </ul>
+            </nav>
+
+
+
     </header>
 
     <main>
+        <div id="wrapper">
         <article>blabla</article>
+        </div>
     </main>
 
     <footer>
     </footer>
-</div>
+
 </body>
 </html>
