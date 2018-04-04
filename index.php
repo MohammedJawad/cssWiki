c518043eb70e0bbd0ef681e3cdbdb4f834a73093
bleyvamoo <blancafer_leyva@hotmail.com>
2018-04-04 12:57:44 -0400

    Css folder Header is done but the heart-icon is missing the link and the menu needs transitions

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
 <head>
     <meta charset="UTF-8"/>
     <title>cssWiki</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
     <link href="style/main.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
+
 </head>
 <body>
 
@@ -21,17 +23,20 @@
                 <li><a href="index.php">Accueil</a></li>
                 <li><a href="catalogue.php">Catalogue</a></li>
                 <li><a href="contact.php">Contactez-Nous</a></li>
-
-
+                <li><a class="fas fa-heart"></a></li>
             </ul>
+
             </nav>
+
 
 
 
     </header>
 
     <main>
-        <div id="wrapper">
+
+
+    <div id="wrapper">
         <article>blabla</article>
         </div>
     </main>
