5db05f40cc3a07b7fbf84881e1032b971a02e3f1
kelgreen <raqguina@hotmail.com>
2018-04-08 00:12:15 -0400

    Add Page Executer: html - css and javascript

--- index.php
+++ index.php
@@ -3,7 +3,6 @@ require_once 'views/page_top.php';
 ?>
         <main>
             <h1>Welcome</h1>
-<<<<<<< HEAD
             <div id="wrapper">
                 <div id="example1">
                     <figure>
@@ -12,8 +11,6 @@ require_once 'views/page_top.php';
                         <span><a class="fas fa-heart"></a></span>
                     </figure>
                 </div>
-=======
->>>>>>> b08e050fb7c0b9db4d83485a32a1874dc41ea754
             </div>
         </main>
 <?php require_once 'views/page_bottom.php';
