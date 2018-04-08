5f27f31d7c1bc55242cd78db0e06075bd8c9a1e5
kelgreen <raqguina@hotmail.com>
2018-04-08 01:47:55 -0400

    Add index's page and exemple 1

--- index.php
+++ index.php
@@ -2,15 +2,112 @@
 require_once 'views/page_top.php';
 ?>
         <main>
-            <h1>Welcome</h1>
-            <div id="wrapper">
-                <div id="example1">
-                    <figure>
-                        <img src="images/bird.png" alt="Bird Example CSS"  id="bird">
-                        <figcaption>Example 1 - A view of the pulpit rock in Norway.</figcaption>
-                        <span><a class="fas fa-heart"></a></span>
-                    </figure>
+            <h1>Bienvenue</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Eu vis dico
+                natum deseruisse, eum an falli mandamus, aeque salutandi usu in. Eum ne liber detracto disputando, ne vim detraxit explicari. No vocent adipiscing
+                concludaturque vix, nullam legere dissentiet has no, quando aperiri ea mea. Ad nec omnium laoreet. Et vero placerat eum, an quis tempor epicuri quo,
+                ad aeterno persius duo.
+            </p>
+            <div id="example1">
+                <div class="wrapper">
+                    <div class="sun"></div>
+                    <div class="cloud">
+                        <div class="cloud1">
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <div class="cloud1 c_shadow">
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="cloud_s">
+                        <div class="cloud1">
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <div class="cloud1 c_shadow">
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="cloud_vs">
+                        <div class="cloud1">
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <div class="cloud1 c_shadow">
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="haze"></div>
+                    <div class="haze_stripe"></div>
+                    <div class="haze_stripe"></div>
+                    <div class="haze_stripe"></div>
+                    <div class="thunder"></div>
+                    <div class="rain">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                    <div class="sleet">
+                        <ul>
+                            <li></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
+                    <div class="text">
+                        <ul>
+                            <li>Mostly Sunny</li>
+                            <li>Partly Sunny</li>
+                            <li>Partly Cloudy</li>
+                            <li>Mostly Cloudy</li>
+                            <li>Cloudy</li>
+                            <li>Hazy</li>
+                            <li>Thunderstorm</li>
+                            <li>Rain</li>
+                            <li>Sleet</li>
+                        </ul>
+                    </div>
                 </div>
+
+            </div>
+            <div id="labtitles">
+                <h3>Animation 1</h3>
+                <a href=""><img src="images/like.png" alt=""></a>
+                <h4>Autor</h4>
             </div>
         </main>
 <?php require_once 'views/page_bottom.php';
