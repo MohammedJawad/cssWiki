bcc0ba7fd2e1aef6e295b5c2554707df679d0963
mjawad <mohammed.jawad.1995@hotmail.com>
2018-04-09 08:06:06 -0400

    contact + css

--- contact.php
+++ contact.php
@@ -19,18 +19,19 @@
         <form id="formContact" method="post">
             <fieldset>
                 <div>
-                    <label for="nom">Nom</label><!--
-                    --><input type="text" name="nom" id="nom" placeholder="Ex: Bob">
+                    <label for="nom">Nom</label>
+                    <input type="text" name="nom" id="nom" placeholder="Ex: Bob">
                 </div>
 
                 <div>
-                    <label for="courriel">Courriel</label><!--
-                    --><input type="email" name="courriel" id="courriel" placeholder="exemple@exemple.com">
+                    <label for="courriel">Courriel</label>
+                    <input type="email" name="courriel" id="courriel" placeholder="exemple@exemple.com">
                 </div>
 
                 <div>
-                    <label for="selectCategorie">Type</label><!--
-                    --><select id="selectCategorie">
+                    <label for="selectCategorie">Catégorie</label>
+                    <select id="selectCategorie">
+                        <option value="-1">Choisissez...</option>
                         <option value="bouton">Bouton</option>
                         <option value="animation">Animation</option>
                         <option value="transition">Transition</option>
@@ -38,8 +39,8 @@
                 </div>
 
                 <div>
-                    <label for="codeTexte">CSS</label><!--
-                    --><textarea id="codeTexte" placeholder="Insérer votre code ici"></textarea>
+                    <label for="codeTexte">CSS</label>
+                    <textarea id="codeTexte" placeholder="Insérer votre code ici"></textarea>
                 </div>
             </fieldset>
             <input type="submit" value="Envoyer">
