0cb059142557cb6d283aec0c6b9afd077ada4f9d
bleyvamoo <blancafer_leyva@hotmail.com>
2018-04-10 11:11:44 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.php
    #	style/main.css
    #	views/page_bottom.php

--- index.php
+++ index.php
@@ -10,24 +10,120 @@ require_once 'views/page_top.php';
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
     <link href="style/main.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Karla" rel="stylesheet">
 
 </head>
     <body>
 
         <main>
-            <div id="wrapper">
-                <div id="banner">
-                    <img src="images/banner.png" alt="banner" id="bannercss">
-                </div>
-                <h1>Check out our pens</h1>
-                <div id="example1">
-                    <figure>
-                    <img src="images/bird.png" alt="Bird Example CSS"  id="bird">
-                    <figcaption>Example 1 - A view of the pulpit rock in Norway.</figcaption>
-                    <span>Blanca Leyva</span>
-                </figure>
-                </div>
+            <h1>Bienvenue</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Eu vis dico
+                natum deseruisse, eum an falli mandamus, aeque salutandi usu in. Eum ne liber detracto disputando, ne vim detraxit explicari. No vocent adipiscing
+                concludaturque vix, nullam legere dissentiet has no, quando aperiri ea mea. Ad nec omnium laoreet. Et vero placerat eum, an quis tempor epicuri quo,
+                ad aeterno persius duo.
+            </p>
 
+            <div id="example1"><a href="catalogue.php">
+                    <div class="wrapper">
+                        <div class="sun"></div>
+                        <div class="cloud">
+                            <div class="cloud1">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                            <div class="cloud1 c_shadow">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="cloud_s">
+                            <div class="cloud1">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                            <div class="cloud1 c_shadow">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <div class="cloud_vs">
+                            <div class="cloud1">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                            <div class="cloud1 c_shadow">
+                                <ul>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                    <li></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="haze"></div>
+                        <div class="haze_stripe"></div>
+                        <div class="haze_stripe"></div>
+                        <div class="haze_stripe"></div>
+                        <div class="thunder"></div>
+                        <div class="rain">
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <div class="sleet">
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                        <div class="text">
+                            <ul>
+                                <li>Mostly Sunny</li>
+                                <li>Partly Sunny</li>
+                                <li>Partly Cloudy</li>
+                                <li>Mostly Cloudy</li>
+                                <li>Cloudy</li>
+                                <li>Hazy</li>
+                                <li>Thunderstorm</li>
+                                <li>Rain</li>
+                                <li>Sleet</li>
+                            </ul>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div id="labtitles">
+                <h3>Animation 1</h3>
+                <a href=""><img src="images/like.png" alt=""></a>
+                <h4>Autor</h4>
+            </div>
 
 
         </main>
