4e510254eb72c0f6ed1588b46aefd07597776e10
bleyvamoo <blancafer_leyva@hotmail.com>
2018-04-16 11:18:44 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.php
    #	style/main.css
    #	views/page_bottom.php

--- views/page_top.php
+++ views/page_top.php
@@ -14,6 +14,7 @@
         <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
         <link href="css/font-awesome.css" rel="stylesheet">
         <script src="jquery/jquery-hamburger.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
         <header>
