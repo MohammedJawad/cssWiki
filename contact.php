3e6755d5681338618a4de6a6bb0d649f4e78aa0b
kelgreen <raqguina@hotmail.com>
2018-04-15 22:22:54 -0400

    Formulaire - PHP messages d'erreur

--- contact.php
+++ contact.php
@@ -64,32 +64,47 @@ if($en_reception){
 
     <main>
         <h1 class="animTitle">Contactez-nous!</h1>
-        <img src="images/banner.png">
         <form id="formContact" method="post">
             <fieldset>
                 <div>
                     <label class="<?= ($en_reception && ! $vld[F_NOM][VK_IS_VALID]) ? 'invalide' : '' ?>" for="nom">Nom</label>
                     <input type="text" name="<?= F_NOM ?>" id="nom" placeholder="Ex: Bob" value="<?= ($en_reception) ? $vld[F_NOM][VK_VALUE] : '' ?>">
+                    <?php
+                    if ($en_reception && !$vld[F_NOM][VK_IS_VALID]) {
+                        echo '<span id="errmsg">', $vld[F_NOM][VK_MSG], '</span>';
+                    }?>
                 </div>
 
                 <div>
                     <label class="<?= ($en_reception && ! $vld[F_COURRIEL][VK_IS_VALID]) ? 'invalide' : '' ?>" for="courriel">Courriel</label>
                     <input type="email" name="<?= F_COURRIEL ?>" id="courriel" placeholder="exemple@exemple.com" value="<?= ($en_reception) ? $vld[F_COURRIEL][VK_VALUE] : '' ?>">
+                    <?php
+                    if ($en_reception && !$vld[F_COURRIEL][VK_IS_VALID]) {
+                        echo '<span id="errmsg">', $vld[F_COURRIEL][VK_MSG], '</span>';
+                    }?>
                 </div>
 
                 <div>
                     <label class="<?= ($en_reception && ! $vld[F_CATEGORIE][VK_IS_VALID]) ? 'invalide' : '' ?>" for="selectCategorie">Catégorie</label>
                     <select id="selectCategorie" name="<?= F_CATEGORIE ?>">
                         <option value="-1" <?= array_key_exists(F_CATEGORIE, $_POST) && $_POST[F_CATEGORIE] === "-1" ? ATTR_SELECTED : '' ?>>Choisissez...</option>
+
                         <option value="bouton" <?= array_key_exists(F_CATEGORIE, $_POST) && $_POST[F_CATEGORIE] === "bouton" ? ATTR_SELECTED : '' ?>>Bouton</option>
                         <option value="animation" <?= array_key_exists(F_CATEGORIE, $_POST) && $_POST[F_CATEGORIE] === "animation" ? ATTR_SELECTED : '' ?>>Animation</option>
                         <option value="transition" <?= array_key_exists(F_CATEGORIE, $_POST) && $_POST[F_CATEGORIE] === "transition" ? ATTR_SELECTED : '' ?>>Transition</option>
-                    </select>
+                    </select><?php
+                    if ($en_reception && !$vld[F_CATEGORIE][VK_IS_VALID]) {
+                        echo '<span id="errmsg">', $vld[F_CATEGORIE][VK_MSG], '</span>';
+                    }?>
                 </div>
 
                 <div>
                     <label class="<?= ($en_reception && ! $vld[F_MESSAGE][VK_IS_VALID]) ? 'invalide' : '' ?>" for="codeTexte">HTML/CSS</label>
                     <textarea name="<?= F_MESSAGE ?>" id="codeTexte" placeholder="Insérer votre code ici"><?= ($en_reception) ? $vld[F_MESSAGE][VK_VALUE] : '' ?></textarea>
+                    <?php
+                    if ($en_reception && !$vld[F_MESSAGE][VK_IS_VALID]) {
+                        echo '<span id="errmsg">', $vld[F_MESSAGE][VK_MSG], '</span>';
+                    }?>
                 </div>
             </fieldset>
             <input type="submit" value="Envoyer">
